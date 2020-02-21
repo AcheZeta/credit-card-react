8c98d5c63ccd7b43ccaf5b15dc6375fc2e49449f
AcheZeta <ache.mariana@gmail.com>
2020-02-20 23:09:14 -0600

    Set Up proyect with Style components

---
+++ src/components/Forms.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const Form = () => (
+    <div>
+        <input type="text" name="cardName" id="name" placeholder="Card Name"/>
+    <input type="number" name="CardNumber" id="number" placeholder="Card Number"/>
+    <input type="password" name="passport" id=""/>
+    </div>
+)
+
+export default Form
